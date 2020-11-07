9644377be74108c507f655f6ecfc224897a9a257
Condal <r0742009@ems.niu.edu.tw>
2020-11-07 22:55:56 +0800

    just init bot.py

---
+++ A_python_bybit_bot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/11/7 下午 10:52
+# @Author : Condal36
+# @Site : 
+# @File : A_python_bybit_bot.py
+# @Software: PyCharm
+import bybit
+
+welcommessage="Welcome trade bot !"
+if __name__=="__main__":
+    print(welcommessage)
+    pass
